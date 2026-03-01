bddb9b4011ed0aa0d937a0d2bd20dac76f7bd707
Nime V R <nimevr109@gmail.com>
2026-03-01 23:51:21 +0530

    Remove Elements val in a array nums

---
+++ src/leetcode/RemoveElements.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+ class RemoveElements {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        RemoveElements solution = new RemoveElements();
+
+        int[] nums1 = {3, 2, 2, 3};
+        int val1 = 3;
+        int k1 = solution.removeElement(nums1, val1);
+        System.out.println("k = " + k1 + ", nums = " + java.util.Arrays.toString(nums1)); // First k1 elements are valid
+
+        int[] nums2 = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val2 = 2;
+        int k2 = solution.removeElement(nums2, val2);
+        System.out.println("k = " + k2 + ", nums = " + java.util.Arrays.toString(nums2)); // First k2 elements are valid
+    }
+}   
