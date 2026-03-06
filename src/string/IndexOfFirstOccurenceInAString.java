7b19e89e4d9332cafacaddd1176654d308580d18
Nime V R <nimevr109@gmail.com>
2026-03-06 19:01:47 +0530

    Add Index Of First Occurence in a string

--- src/string/IndexOfFirstOccurenceInAString.java
+++ src/string/IndexOfFirstOccurenceInAString.java
@@ -1,10 +1,27 @@
 package string;
 
+
+
 public class IndexOfFirstOccurenceInAString {
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+        
+        for (int i = 0; i <= m - n; i++) {
+            if (haystack.substring(i, i + n).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+    	IndexOfFirstOccurenceInAString solution = new IndexOfFirstOccurenceInAString();
 
-	}
+        System.out.println(solution.strStr("sadbutsad", "sad")); 
 
-}
+        System.out.println(solution.strStr("leetcode", "leeto")); 
+
+        System.out.println(solution.strStr("hello", "ll")); 
+    }
+}   
